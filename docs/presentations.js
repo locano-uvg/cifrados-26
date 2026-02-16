3ff2ca5b01ba9787c2dbfb528afef7f2c81195ce
Ludwing Cano <ludwing238@gmail.com>
2026-02-16 16:07:13 -0600

    feat: Adding block ciphers

--- docs/presentations.js
+++ docs/presentations.js
@@ -1,99 +1,100 @@
 export const presentations = [
-    {
-        title: "Bienvenida",
-        description: "Introducción al curso de Cifrados",
-        path: "temas/welcome/index.html",
-        image: "temas/welcome/welcome.png",
-        active: true
-    },
-    {
-        title: "Cifrados",
-        description: "Conceptos básicos",
-        path: "temas/cifrados/index.html",
-        image: "temas/cifrados/assets/cipher.png",
-        active: true
-    },
-    {
-        title: "Criptografía",
-        description: "Algoritmos históricos",
-        path: "temas/criptografia/index.html",
-        image: "temas/criptografia/assets/criptografia.png",
-        active: true
-    },
-    {
-        title: "Cifrados Simétricos",
-        description: "Introducción a los cifrados simétricos",
-        path: "temas/simetricos/index.html",
-        image: "temas/simetricos/assets/simetricos.png",
-        active: true
-    },
-    {
-        title: "Stream Ciphers",
-        description: "Cifrados de flujo",
-        path: "temas/simetricos/stream/index.html",
-        image: "temas/simetricos/assets/stream.png",
-        active: true
-    },
-    {
-        title: "Algoritmos Stream Ciphers",
-        description: "OTP, ChaCha20, Salsa20...",
-        path: "temas/simetricos/stream/algoritmos.html",
-        image: "temas/simetricos/assets/otp.png",
-        active: true
-    },
-    {
-        title: "Block Ciphers",
-        description: "Cifrados de bloque",
-        path: "temas/simetricos/block/index.html",
-        image: "temas/simetricos/assets/block.png",
-        active: false
-    }, {
-        title: "Algoritmos Block Ciphers",
-        description: "AES, DES, 3DES...",
-        path: "temas/simetricos/block/algoritmos.html",
-        image: "temas/simetricos/assets/block_al.png",
-        active: false
-    },
-    {
-        title: "Cifrados Asimétricos",
-        description: "Introducción a los cifrados asimétricos",
-        path: "temas/asimetricos/index.html",
-        image: "temas/asimetricos/assets/asimetricos.png",
-        active: false
-    },
-    {
-        title: "Hashes",
-        description: "Funciones hash",
-        path: "temas/asimetricos/Hashes/index.html",
-        image: "temas/asimetricos/assets/hash.png",
-        active: false
-    },
-    {
-        title: "Firmas Digitales",
-        description: "Firmas digitales",
-        path: "temas/asimetricos/Firmas/index.html",
-        image: "temas/asimetricos/assets/firma1.jpg",
-        active: false
-    },
-    {
-        title: "RSA",
-        description: "Algoritmo RSA",
-        path: "temas/asimetricos/RSA/index.html",
-        image: "temas/asimetricos/assets/rsa1.png",
-        active: false
-    },
-    {
-        title: "ECC",
-        description: "Algoritmo ECC",
-        path: "temas/asimetricos/ECC/index.html",
-        image: "temas/asimetricos/assets/ecc.png",
-        active: false
-    },
-    {
-        title: "Algoritmos Asimétricos",
-        description: "Comparación de Algoritmos asimétricos",
-        path: "temas/asimetricos/Algoritmos/index.html",
-        image: "temas/asimetricos/assets/algoritmos.jpeg",
-        active: false
-    }
+  {
+    title: "Bienvenida",
+    description: "Introducción al curso de Cifrados",
+    path: "temas/welcome/index.html",
+    image: "temas/welcome/welcome.png",
+    active: true,
+  },
+  {
+    title: "Cifrados",
+    description: "Conceptos básicos",
+    path: "temas/cifrados/index.html",
+    image: "temas/cifrados/assets/cipher.png",
+    active: true,
+  },
+  {
+    title: "Criptografía",
+    description: "Algoritmos históricos",
+    path: "temas/criptografia/index.html",
+    image: "temas/criptografia/assets/criptografia.png",
+    active: true,
+  },
+  {
+    title: "Cifrados Simétricos",
+    description: "Introducción a los cifrados simétricos",
+    path: "temas/simetricos/index.html",
+    image: "temas/simetricos/assets/simetricos.png",
+    active: true,
+  },
+  {
+    title: "Stream Ciphers",
+    description: "Cifrados de flujo",
+    path: "temas/simetricos/stream/index.html",
+    image: "temas/simetricos/assets/stream.png",
+    active: true,
+  },
+  {
+    title: "Algoritmos Stream Ciphers",
+    description: "OTP, ChaCha20, Salsa20...",
+    path: "temas/simetricos/stream/algoritmos.html",
+    image: "temas/simetricos/assets/otp.png",
+    active: true,
+  },
+  {
+    title: "Block Ciphers",
+    description: "Cifrados de bloque",
+    path: "temas/simetricos/block/index.html",
+    image: "temas/simetricos/assets/block.png",
+    active: true,
+  },
+  {
+    title: "Algoritmos Block Ciphers",
+    description: "AES, DES, 3DES...",
+    path: "temas/simetricos/block/algoritmos.html",
+    image: "temas/simetricos/assets/block_al.png",
+    active: true,
+  },
+  {
+    title: "Cifrados Asimétricos",
+    description: "Introducción a los cifrados asimétricos",
+    path: "temas/asimetricos/index.html",
+    image: "temas/asimetricos/assets/asimetricos.png",
+    active: false,
+  },
+  {
+    title: "Hashes",
+    description: "Funciones hash",
+    path: "temas/asimetricos/Hashes/index.html",
+    image: "temas/asimetricos/assets/hash.png",
+    active: false,
+  },
+  {
+    title: "Firmas Digitales",
+    description: "Firmas digitales",
+    path: "temas/asimetricos/Firmas/index.html",
+    image: "temas/asimetricos/assets/firma1.jpg",
+    active: false,
+  },
+  {
+    title: "RSA",
+    description: "Algoritmo RSA",
+    path: "temas/asimetricos/RSA/index.html",
+    image: "temas/asimetricos/assets/rsa1.png",
+    active: false,
+  },
+  {
+    title: "ECC",
+    description: "Algoritmo ECC",
+    path: "temas/asimetricos/ECC/index.html",
+    image: "temas/asimetricos/assets/ecc.png",
+    active: false,
+  },
+  {
+    title: "Algoritmos Asimétricos",
+    description: "Comparación de Algoritmos asimétricos",
+    path: "temas/asimetricos/Algoritmos/index.html",
+    image: "temas/asimetricos/assets/algoritmos.jpeg",
+    active: false,
+  },
 ];
